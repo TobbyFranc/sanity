fe94943748afafa7f78d318c9ac9a68cd973ab8f
Tobi Francis Ogounz <77078820+TobbyFranc@users.noreply.github.com>
2025-12-16 09:54:33 +0100

    sella site integration

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,6 +1,9 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+// import blockContent from './blockContent'
+// import category from './category'
+// import post from './post'
+// import author from './author'
+import blogPost from './post';
+export const schemaTypes = [blogPost];
 
-export const schemaTypes = [post, author, category, blockContent]
+
+// export const schemaTypes = [post, author, category, blockContent]
